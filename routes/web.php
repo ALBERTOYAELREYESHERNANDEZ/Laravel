cbf4445b1c6a6efd2f59abf0836942f16b5230f7
Christian <christianreyeshdz117@gmail.com>
2023-10-17 18:01:29 -0600

    Primer commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EstadoController;
+use App\Http\Controllers\ConfirmadoController;
+// use App\Http\Controllers\EstadoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,10 +25,16 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+require __DIR__.'/auth.php';
+// EstadoController::class
+Route::resource('/estados',EstadoController::class);
+Route::resource('/confirmados',ConfirmadoController::class);
+// Route::resource('/confirmados', )
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+
